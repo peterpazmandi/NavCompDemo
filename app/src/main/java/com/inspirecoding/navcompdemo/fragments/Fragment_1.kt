994f9a703f4118cc9b8efe02fc1a425942d0c30c
inspire-coding <+PaZ+PeTi+88113+>
2020-03-14 10:16:09 +0100

    Add library and make action

--- app/src/main/java/com/inspirecoding/navcompdemo/fragments/Fragment_1.kt
+++ app/src/main/java/com/inspirecoding/navcompdemo/fragments/Fragment_1.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
 import com.inspirecoding.navcompdemo.R
+import kotlinx.android.synthetic.main.fragment_1.*
 
 class Fragment_1 : Fragment()
 {
@@ -14,5 +17,16 @@ class Fragment_1 : Fragment()
     {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_1, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?)
+    {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_fragment_1.setOnClickListener {
+            val navController: NavController = Navigation.findNavController(it)
+            val action = Fragment_1Directions.actionFragment1ToFragment2()
+            navController.navigate(action)
+        }
     }
 }
--- app/src/main/java/com/inspirecoding/navcompdemo/fragments/Fragment_2.kt
+++ app/src/main/java/com/inspirecoding/navcompdemo/fragments/Fragment_2.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
 import com.inspirecoding.navcompdemo.R
+import kotlinx.android.synthetic.main.fragment_2.*
 
 class Fragment_2 : Fragment()
 {
@@ -14,5 +17,16 @@ class Fragment_2 : Fragment()
     {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_2, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?)
+    {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_fragment_2.setOnClickListener {
+            val navController: NavController = Navigation.findNavController(it)
+            navController.navigate(Fragment_2Directions.actionFragment2ToFragment3())
+        }
     }
 }
