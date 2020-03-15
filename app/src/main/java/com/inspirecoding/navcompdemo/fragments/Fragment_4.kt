dc7eb61ce4a3040b35edf1df02100e089e69addb
inspire-coding <+PaZ+PeTi+88113+>
2020-03-15 21:45:56 +0100

    Open Fragment_5

--- app/src/main/java/com/inspirecoding/navcompdemo/fragments/Fragment_4.kt
+++ app/src/main/java/com/inspirecoding/navcompdemo/fragments/Fragment_4.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
 import com.inspirecoding.navcompdemo.R
+import kotlinx.android.synthetic.main.fragment_4.*
 
 class Fragment_4 : Fragment()
 {
@@ -14,5 +17,15 @@ class Fragment_4 : Fragment()
     {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_4, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?)
+    {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_fragment_4.setOnClickListener {
+            val navController: NavController = Navigation.findNavController(it)
+            navController.navigate(Fragment_4Directions.actionFragment4ToFragment5())
+        }
     }
 }
