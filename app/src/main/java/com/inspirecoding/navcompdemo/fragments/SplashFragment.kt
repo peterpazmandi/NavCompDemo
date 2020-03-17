caa33934244e64eef48198de5a89b92234f10869
inspire-coding <+PaZ+PeTi+88113+>
2020-03-17 16:55:00 +0100

    The starter project for the Managing back stack chapter

---
+++ app/src/main/java/com/inspirecoding/navcompdemo/fragments/SplashFragment.kt
@@ -0,0 +1,32 @@
+package com.inspirecoding.navcompdemo.fragments
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import com.inspirecoding.navcompdemo.R
+
+class SplashFragment : Fragment()
+{
+    override fun onStart()
+    {
+        super.onStart()
+
+        (activity as AppCompatActivity).supportActionBar?.hide()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View?
+    {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?)
+    {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
