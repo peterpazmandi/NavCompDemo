85aa07b84ef9c135b9cc726fade97b84f9c83f62
inspire-coding <+PaZ+PeTi+88113+>
2020-03-28 12:40:43 +0100

    The final project of the bottom navigation bar

--- app/src/main/java/com/inspirecoding/navcompdemo/MainActivity.kt
+++ app/src/main/java/com/inspirecoding/navcompdemo/MainActivity.kt
@@ -2,12 +2,63 @@ package com.inspirecoding.navcompdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity()
 {
+    private lateinit var navController: NavController
+
+    private val appBarConfiguration by lazy {
+        AppBarConfiguration(
+            setOf(
+                R.id.fragment_1,
+                R.id.fragment_2,
+                R.id.fragment_3,
+                R.id.fragment_4
+            )
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        //Getting the Navigation Controller
+        navController = Navigation.findNavController(this, R.id.container)
+
+        //Setting the navigation controller to Bottom Nav
+        bottom_nav.setupWithNavController(navController)
+
+        //Setting up the action bar
+        NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id)
+            {
+                R.id.fragment_5 -> hideBottomNav()
+                else -> showBottomNav()
+            }
+        }
+    }
+
+    private fun showBottomNav()
+    {
+        bottom_nav.visibility = View.VISIBLE
+    }
+
+    private fun hideBottomNav()
+    {
+        bottom_nav.visibility = View.GONE
     }
 }
--- app/src/main/java/com/inspirecoding/navcompdemo/fragments/Fragment_4.kt
+++ app/src/main/java/com/inspirecoding/navcompdemo/fragments/Fragment_4.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 
 import com.inspirecoding.navcompdemo.R
+import kotlinx.android.synthetic.main.fragment_4.*
 
 class Fragment_4 : Fragment()
 {
@@ -14,5 +16,17 @@ class Fragment_4 : Fragment()
     {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_4, container, false)
+    }
+
+    // 1
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?)
+    {
+        super.onViewCreated(view, savedInstanceState)
+
+        // 2
+        btn_fragment_4.setOnClickListener {
+            // 3
+            findNavController().navigate(R.id.action_fragment_4_to_fragment_5)
+        }
     }
 }
